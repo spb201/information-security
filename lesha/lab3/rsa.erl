57e7aa9abff98587cd68be3b818be4af439b8998
Lesha Koss <regiusprod@gmail.com>
2016-04-08 12:41:53 +0600

    Fix some bugs

--- lesha/lab3/rsa.erl
+++ lesha/lab3/rsa.erl
@@ -10,6 +10,23 @@ bin_to_int(undefined) ->
     undefined.
 
 
+
+trim(Bin) when is_binary(Bin) ->
+        list_to_binary(trim(binary_to_list(Bin)));
+trim(String) when is_list(String) ->
+    F = fun(Elem) ->
+        case Elem of
+            0 -> true;
+            Otherwise -> false
+        end
+    end,
+    String2 = lists:dropwhile(F, String),
+    lists:reverse(lists:dropwhile(F, lists:reverse
+(String2))).
+
+
+
+
 encrypt(Size, Key1, Key2, Bin) ->
     Size1 = Size-1,
     Y = Size1 - (bit_size(Bin) rem Size1),
@@ -32,8 +49,10 @@ decrypt(Size, Key1, Key2, Bin) ->
     lists:foldl(F, <<>>, FileBlockified).
 
 
-main(["e", SizeStr, Key1Str, Key2Str, FileFrom, FileTo]) ->
+main(["e", FileKey, FileFrom, FileTo]) ->
     {ok, Bin} = file:read_file(FileFrom),
+    {ok, KeyBin} = file:read_file(FileKey),
+    [SizeStr, Key1Str, Key2Str] = string:tokens(binary:bin_to_list(KeyBin), ":"),
     Size = list_to_integer(SizeStr),
     Key1 = list_to_integer(Key1Str),
     Key2 = list_to_integer(Key2Str),
@@ -42,14 +61,17 @@ main(["e", SizeStr, Key1Str, Key2Str, FileFrom, FileTo]) ->
     file:write_file(FileTo, <<Encrypted/bits, 0:Y>>);
 
 
-main(["d", SizeStr, Key1Str, Key2Str, FileFrom, FileTo]) ->
+main(["d", FileKey, FileFrom, FileTo]) ->
     {ok, Bin} = file:read_file(FileFrom),
+    {ok, KeyBin} = file:read_file(FileKey),
+    [SizeStr, Key1Str, Key2Str] = string:tokens(binary:bin_to_list(KeyBin), ":"),
     Size = list_to_integer(SizeStr),
     Key1 = list_to_integer(Key1Str),
     Key2 = list_to_integer(Key2Str),
     Decrypted = decrypt(Size, Key1, Key2, Bin),
     Y = 8 - (bit_size(Decrypted) rem 8),
-    file:write_file(FileTo, <<Decrypted/bits, 0:Y>>);
+    FileWhat = <<Decrypted/bits, 0:Y>>,
+    file:write_file(FileTo, trim(FileWhat));
 
 
 main(_) ->
